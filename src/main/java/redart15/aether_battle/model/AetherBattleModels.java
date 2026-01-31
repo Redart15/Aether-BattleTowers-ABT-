5c560c7f9caa86932ad02ed4169affac36558a49
Redart15 <redart15@users.noreply.github.com>
2026-01-31 18:06:11 +0100

    entrypoint for models

---
+++ src/main/java/redart15/aether_battle/model/AetherBattleModels.java
@@ -0,0 +1,38 @@
+package redart15.aether_battle.model;
+
+import net.minecraft.client.render.EntityRenderDispatcher;
+import net.minecraft.client.render.TileEntityRenderDispatcher;
+import net.minecraft.client.render.block.color.BlockColorDispatcher;
+import net.minecraft.client.render.block.model.BlockModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelStandard;
+import redart15.aether_battle.item.AetherBattleItems;
+import turniplabs.halplibe.util.ModelEntrypoint;
+
+public class AetherBattleModels implements ModelEntrypoint {
+	@Override
+	public void initBlockModels(BlockModelDispatcher blockModelDispatcher) {
+
+	}
+
+	@Override
+	public void initItemModels(ItemModelDispatcher itemModelDispatcher) {
+		itemModelDispatcher.addDispatch(new ItemModelStandard(AetherBattleItems.DICINATOR, (String) null).setIcon("aether:item/amber"));
+
+	}
+
+	@Override
+	public void initEntityModels(EntityRenderDispatcher entityRenderDispatcher) {
+
+	}
+
+	@Override
+	public void initTileEntityModels(TileEntityRenderDispatcher tileEntityRenderDispatcher) {
+
+	}
+
+	@Override
+	public void initBlockColors(BlockColorDispatcher blockColorDispatcher) {
+
+	}
+}
