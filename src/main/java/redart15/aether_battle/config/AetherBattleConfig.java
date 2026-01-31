1a116f25bd9203f2af8eebef934dbb73c3b64233
Redart15 <redart15@users.noreply.github.com>
2026-01-31 18:02:39 +0100

    setup config entry

---
+++ src/main/java/redart15/aether_battle/config/AetherBattleConfig.java
@@ -0,0 +1,30 @@
+package redart15.aether_battle.config;
+
+public class AetherBattleConfig {
+	private static int BLOCK_ID_STARTING_FROM = 11000;
+	private static int ITEM_ID_STARTING_FROM = 27000;
+	public static int currentBlockID;
+	public static int currentItemID;
+
+	private static boolean init = false;
+
+	public static void init(){
+		if(init) return;
+		init = true;
+		loadProperties();
+	}
+
+	private static void loadProperties(){
+		currentItemID = ITEM_ID_STARTING_FROM;
+		currentBlockID = BLOCK_ID_STARTING_FROM;
+	}
+
+
+	public static int nextItemID() {
+		return currentItemID++;
+	}
+
+	public static int nextBlockID() {
+		return currentBlockID++;
+	}
+}
