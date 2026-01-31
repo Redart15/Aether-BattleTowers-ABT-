4190fa56b865b3e22278e2a69165ef49ddc8dca6
Redart15 <redart15@users.noreply.github.com>
2026-01-31 18:06:29 +0100

    entrypoint for entities

---
+++ src/main/java/redart15/aether_battle/entity/AetherBattleEntities.java
@@ -0,0 +1,24 @@
+package redart15.aether_battle.entity;
+
+import net.minecraft.core.util.collection.NamespaceID;
+import redart15.aether_battle.entity.projectile.ProjectileChaoticWand;
+import teamport.aether.entity.projectile.ProjectileDart;
+import turniplabs.halplibe.helper.EntityHelper;
+
+import static redart15.aether_battle.AetherBattleMod.MOD_ID;
+
+public class AetherBattleEntities {
+	private static boolean hasInit = false;
+
+	public static void init() {
+		if (!hasInit) {
+			hasInit = true;
+			initProjectile();
+		}
+
+	}
+
+	public static void initProjectile() {
+		EntityHelper.createEntity(ProjectileChaoticWand.class, NamespaceID.getPermanent(MOD_ID, "chaotic_wand"), null);
+	}
+}
