0534e15e426ca0ef5cf7515624a40df181476614
Redart15 <redart15@users.noreply.github.com>
2026-02-08 18:40:44 +0100

    projectile renderer

---
+++ src/main/java/redart15/aether_battle/model/EntityRendererChaoticWandDart.java
@@ -0,0 +1,16 @@
+package redart15.aether_battle.model;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.tessellator.Tessellator;
+import redart15.aether_battle.entity.projectile.ProjectileChaotic;
+
+@Environment(EnvType.CLIENT)
+public class EntityRendererChaoticWandDart extends EntityRenderer<ProjectileChaotic> {
+	public void render(Tessellator tessellator, ProjectileChaotic dart, double x, double y, double z, float yaw, float partialTick) {
+		/* the particles are a better indicator of where the projectile is*/
+	}
+
+
+}
