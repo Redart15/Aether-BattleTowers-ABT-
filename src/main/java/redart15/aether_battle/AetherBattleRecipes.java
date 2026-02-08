8fc7234b7f2889f4e8273412997d361aa4fb106b
Redart15 <redart15@users.noreply.github.com>
2026-02-08 18:41:26 +0100

    move the recipe entrypoint in directories

---
+++ src/main/java/redart15/aether_battle/AetherBattleRecipes.java
@@ -0,0 +1,15 @@
+package redart15.aether_battle;
+
+import turniplabs.halplibe.util.RecipeEntrypoint;
+
+public class AetherBattleRecipes implements RecipeEntrypoint {
+	@Override
+	public void onRecipesReady() {
+
+	}
+
+	@Override
+	public void initNamespaces() {
+
+	}
+}
