680db29cb9c9aec6dbafb8ff8fb165232c315f11
Redart15 <redart15@users.noreply.github.com>
2026-02-19 22:06:10 +0100

    added item model for the items

--- src/main/java/redart15/aether_battle/model/AetherBattleModels.java
+++ src/main/java/redart15/aether_battle/model/AetherBattleModels.java
@@ -5,8 +5,10 @@ import net.minecraft.client.render.TileEntityRenderDispatcher;
 import net.minecraft.client.render.block.color.BlockColorDispatcher;
 import net.minecraft.client.render.block.model.BlockModelDispatcher;
 import net.minecraft.client.render.item.model.ItemModelDispatcher;
+import net.minecraft.client.render.item.model.ItemModelStandard;
 import redart15.aether_battle.entity.projectile.ProjectileChaotic;
 import redart15.aether_battle.item.AetherBattleItems;
+import teamport.aether.AetherMod;
 import turniplabs.halplibe.helper.ModelHelper;
 import turniplabs.halplibe.util.ModelEntrypoint;
 
@@ -25,6 +27,12 @@ public class AetherBattleModels implements ModelEntrypoint {
 			.setIcon(MOD_ID + ":item/chaotic_wand/wand")
 			.setFull3D()
 			.setFullBright());
+
+		itemModelDispatcher.addDispatch(new ItemModelStandard(AetherBattleItems.CONGEALED_BLOOD, null)
+			.setIcon(MOD_ID + ":item/congealed_blood"));
+
+		itemModelDispatcher.addDispatch(new ItemModelStandard(AetherBattleItems.CRIMSON_GEM, null)
+			.setIcon(MOD_ID + ":item/blood_stone"));
 	}
 
 	@Override
@@ -34,11 +42,11 @@ public class AetherBattleModels implements ModelEntrypoint {
 
 	@Override
 	public void initTileEntityModels(TileEntityRenderDispatcher tileEntityRenderDispatcher) {
-
+		/* no need */
 	}
 
 	@Override
 	public void initBlockColors(BlockColorDispatcher blockColorDispatcher) {
-
+		/* no need */
 	}
 }
