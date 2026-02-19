880482c05968ddc01b37293c84c72fdcf3428fbd
Redart15 <redart15@users.noreply.github.com>
2026-02-19 22:04:18 +0100

    new items

--- src/main/java/redart15/aether_battle/item/AetherBattleItems.java
+++ src/main/java/redart15/aether_battle/item/AetherBattleItems.java
@@ -5,11 +5,13 @@ import net.minecraft.core.item.Item;
 import net.minecraft.core.item.material.ToolMaterial;
 import net.minecraft.core.item.tag.ItemTags;
 import redart15.aether_battle.config.AetherBattleConfig;
+import teamport.aether.item.AetherItemTags;
 import turniplabs.halplibe.helper.ItemBuilder;
 
 import static redart15.aether_battle.AetherBattleMod.MOD_ID;
 
 public class AetherBattleItems {
+	public static Item CONGEALED_BLOOD;
 	public static Item DICINATOR;
 	public static Item CRIMSON_GEM;
 
@@ -26,5 +28,12 @@ public class AetherBattleItems {
 		DICINATOR = new ItemBuilder(MOD_ID)
 			.addTags(ItemTags.PREVENT_CREATIVE_MINING, ItemTags.PREVENT_LEFT_CLICK_INTERACTIONS)
 			.build(new ItemChaoticWand("chaotic.wand", itemKey("chaotic_wand"), AetherBattleConfig.nextItemID(), ToolMaterial.diamond));
+
+		CRIMSON_GEM = new ItemBuilder(MOD_ID)
+			.build(new ItemCrimsonGem("crimson.gem", itemKey("crimson_gem"), AetherBattleConfig.nextItemID(), "blood_gem"));
+
+		CONGEALED_BLOOD = new ItemBuilder(MOD_ID)
+			.addTags(AetherItemTags.IMMUNE_TO_FIRE_DAMAGE, AetherItemTags.IMMUNE_TO_BLAST_DAMAGE)
+			.build(new ItemCongealedBlood("congealed.blood", itemKey("congealed_blood"), AetherBattleConfig.nextItemID()));
 	}
 }
