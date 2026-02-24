331e5b08bec570f2a1df9ae7cae0426a5b87da09
Redart15 <redart15@users.noreply.github.com>
2026-02-24 15:03:52 +0100

    renamed the items

--- src/main/java/redart15/aether_battle/item/AetherBattleItems.java
+++ src/main/java/redart15/aether_battle/item/AetherBattleItems.java
@@ -12,25 +12,25 @@ import static redart15.aether_battle.AetherBattleMod.MOD_ID;
 
 public class AetherBattleItems {
 	public static Item CONGEALED_BLOOD;
-	public static Item DICINATOR;
-	public static Item CRIMSON_GEM;
+	public static Item CHAOTIC_WAND;
+	public static Item BLOODSTONE;
 
 	private static boolean init = false;
 	private AetherBattleItems(){}
 
 	public static String itemKey(String string) {
-		return "aether:item/" + string;
+		return MOD_ID + ":item/" + string;
 	}
 
 	public static void init(){
 		if(init) return;
 		init = true;
-		DICINATOR = new ItemBuilder(MOD_ID)
+		CHAOTIC_WAND = new ItemBuilder(MOD_ID)
 			.addTags(ItemTags.PREVENT_CREATIVE_MINING, ItemTags.PREVENT_LEFT_CLICK_INTERACTIONS)
 			.build(new ItemChaoticWand("chaotic.wand", itemKey("chaotic_wand"), AetherBattleConfig.nextItemID(), ToolMaterial.diamond));
 
-		CRIMSON_GEM = new ItemBuilder(MOD_ID)
-			.build(new ItemCrimsonGem("crimson.gem", itemKey("crimson_gem"), AetherBattleConfig.nextItemID(), "blood_gem"));
+		BLOODSTONE = new ItemBuilder(MOD_ID)
+			.build(new ItemCrimsonGem("bloodstone", itemKey("bloodstone"), AetherBattleConfig.nextItemID(), "bloodstone"));
 
 		CONGEALED_BLOOD = new ItemBuilder(MOD_ID)
 			.addTags(AetherItemTags.IMMUNE_TO_FIRE_DAMAGE, AetherItemTags.IMMUNE_TO_BLAST_DAMAGE)
