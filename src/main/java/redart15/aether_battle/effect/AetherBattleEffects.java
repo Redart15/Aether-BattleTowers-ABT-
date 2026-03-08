20855eca674e6dd6813148accf1cf7d1bc17e997
Redart15 <redart15@users.noreply.github.com>
2026-03-08 11:03:30 +0100

    new Effects and Attributes

--- src/main/java/redart15/aether_battle/effect/AetherBattleEffects.java
+++ src/main/java/redart15/aether_battle/effect/AetherBattleEffects.java
@@ -1,14 +1,21 @@
 package redart15.aether_battle.effect;
 
 import net.minecraft.core.Global;
+import sunsetsatellite.catalyst.CatalystEffects;
+import sunsetsatellite.catalyst.effects.api.attribute.Attribute;
+import sunsetsatellite.catalyst.effects.api.attribute.Attributes;
+import sunsetsatellite.catalyst.effects.api.attribute.type.IntAttribute;
 import sunsetsatellite.catalyst.effects.api.effect.Effect;
 import sunsetsatellite.catalyst.effects.api.effect.EffectTimeType;
 import sunsetsatellite.catalyst.effects.api.effect.Effects;
 import sunsetsatellite.catalyst.effects.api.effect.render.EffectRenderer;
 import sunsetsatellite.catalyst.effects.api.effect.render.EffectRendererDispatcher;
+import sunsetsatellite.catalyst.effects.api.modifier.ModifierType;
+import sunsetsatellite.catalyst.effects.api.modifier.type.IntModifier;
 import turniplabs.halplibe.helper.EnvironmentHelper;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import static redart15.aether_battle.AetherBattleMod.MOD_ID;
 
@@ -16,6 +23,8 @@ public class AetherBattleEffects {
 	private static boolean hasInit = false;
 	public  static Effect regeneration;
 	public static Effect stoneSkin;
+	public static Attribute stoneSkinAttributes = new IntAttribute("attribute.stone.skin", 0).setAsDefault();
+
 
 
 	private AetherBattleEffects() { /* no need*/ }
@@ -42,17 +51,21 @@ public class AetherBattleEffects {
 			.setDurationIncrease(4 * Global.TICKS_PER_SECOND);
 
 		stoneSkin = new Effect(
-			"effect." + MOD_ID + ".stone.skin",
+			"effect." + MOD_ID + ".stone_skin",
 			MOD_ID + ":stone_skin",
-			new ArrayList<>(),
-			EffectTimeType.PERMANENT, 30
-		);
+			Collections.singletonList(new IntModifier(stoneSkinAttributes, ModifierType.ADD, 1)),
+			EffectTimeType.PERMANENT,
+			10
+		)
+			.setPersistent();
 	}
 
 	private static void registerEffects() {
 		Effects effects = Effects.getInstance();
+		Attributes attributes = Attributes.getInstance();
 		effects.register(regeneration.id, regeneration);
 		effects.register(stoneSkin.id, stoneSkin);
+		attributes.register(MOD_ID + ":stone_skin", stoneSkinAttributes);
 	}
 
 	private static void assignEffectRenderers() {
